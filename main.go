f89d21db175374c1b967773cb7e9a0b1f09f27c3
ndphu <ngdacphu.khtn@gmail.com>
2017-02-28 21:51:11 +0700

    apply cli app arguments and watch all file matching the pattern

--- main.go
+++ main.go
@@ -3,18 +3,19 @@ package main
 import (
 	"crypto/md5"
 	"fmt"
+	"github.com/urfave/cli"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path"
+	"regexp"
 	"strings"
 	"time"
 )
 
 var (
-	WatchFile        = "main.go"
-	WorkDir          = "./"
 	FileHash         = ""
 	SleepDelay int64 = 500
 
@@ -69,23 +70,76 @@ func OnFileChange() {
 
 func main() {
 	defer KillExistingProcess()
-	WorkDir, err := os.Getwd()
-	if err != nil {
-		log.Fatal("Failed to get current dir", err)
-	}
-	log.Printf("Watching file %s in %s\n", WatchFile, WorkDir)
-	for {
-		if b, err := ComputeMd5(path.Join(WorkDir, WatchFile)); err != nil {
-			log.Printf("Err: %v\n", err)
-		} else {
-			newHash := fmt.Sprintf("%x", b)
-			if strings.Compare(newHash, FileHash) != 0 {
-				log.Printf("Change detected\n")
-				OnFileChange()
-			}
-			FileHash = newHash
-		}
-		time.Sleep(100 * time.Millisecond)
+
+	app := cli.NewApp()
+
+	app.Name = "Go Watcher"
+	app.Usage = "Monitor go source files and auto rebuild & reload the app when any source file is changed"
+	app.Author = "Phu Nguyen <ngdacphu.khtn@gmail.com>"
+	app.Version = "0.0.1"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "workdir",
+			Value: "./",
+			Usage: "monitoring directory",
+		},
+		cli.StringFlag{
+			Name:  "pattern",
+			Value: ".*\\.go$",
+			Usage: "pattern for matching the source file",
+		},
 	}
 
+	app.Action = func(c *cli.Context) error {
+		WorkDir := c.String("workdir")
+		Pattern := c.String("pattern")
+		log.Printf("Using working directory %s\n", WorkDir)
+		os.Chdir(WorkDir)
+		cacheMap := make(map[string]string)
+		for {
+			changed := false
+			// TODO handle file removal
+			sourceFiles := ListSourceFile(WorkDir, Pattern)
+			for _, f := range sourceFiles {
+				h, _ := ComputeMd5(f)
+				newHash := fmt.Sprintf("%x", h)
+				oldHash, exists := cacheMap[f]
+				if !exists || strings.Compare(oldHash, newHash) != 0 {
+					changed = true
+					cacheMap[f] = newHash
+				}
+			}
+
+			if changed {
+				OnFileChange()
+			}
+			time.Sleep(500)
+		}
+
+		return nil
+	}
+
+	app.Run(os.Args)
+
+}
+
+func ListSourceFile(baseDir string, pattern string) []string {
+	result := make([]string, 0)
+	files, err := ioutil.ReadDir(baseDir)
+	if err != nil {
+		return make([]string, 0)
+	}
+	for _, e := range files {
+		if e.IsDir() {
+			for _, _e := range ListSourceFile(path.Join(baseDir, e.Name()), pattern) {
+				result = append(result, _e)
+			}
+		} else {
+			match, _ := regexp.MatchString(pattern, e.Name())
+			if match {
+				result = append(result, path.Join(baseDir, e.Name()))
+			}
+		}
+	}
+	return result
 }
